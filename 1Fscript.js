739e4a79bf7243847d416c68db83a484d214081e
kumadesu2999 <a240374t@reitaku.jp>
2025-01-23 05:51:32 +0900

    スタイルパネルのクラス名を統一し、複数のスライドパネルの機能を追加しました。また、校舎タイトルのスタイルを変更しました。

--- 1Fscript.js
+++ 1Fscript.js
@@ -7,3 +7,92 @@ document.getElementById('close-button').addEventListener('click', function () {
   const details = document.getElementById('slide-panel');
   details.classList.remove('show');
 });
+
+document.getElementById('btn-3102').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel2');
+  details.classList.toggle('show2');
+});
+
+document.getElementById('close-button2').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel2');
+  details.classList.remove('show2');
+});
+
+document.getElementById('btn-3103').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel3');
+  details.classList.toggle('show3');
+});
+
+document.getElementById('close-button3').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel3');
+  details.classList.remove('show3');
+});
+
+document.getElementById('btn-3104').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel4');
+  details.classList.toggle('show4');
+});
+
+document.getElementById('close-button4').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel4');
+  details.classList.remove('show4');
+});
+
+document.getElementById('btn-Hall').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel5');
+  details.classList.toggle('show5');
+});
+
+document.getElementById('close-button5').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel5');
+  details.classList.remove('show5');
+});
+
+document.getElementById('btn-hina1f').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel6');
+  details.classList.toggle('show6');
+});
+
+document.getElementById('close-button6').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel6');
+  details.classList.remove('show6');
+});
+
+document.getElementById('btn-Lounge').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel7');
+  details.classList.toggle('show7');
+});
+
+document.getElementById('close-button7').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel7');
+  details.classList.remove('show7');
+});
+document.getElementById('btn-Arena').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel8');
+  details.classList.toggle('show8');
+});
+
+document.getElementById('close-button8').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel8');
+  details.classList.remove('show8');
+});
+
+document.getElementById('btn-Study').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel9');
+  details.classList.toggle('show9');
+});
+
+document.getElementById('close-button9').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel9');
+  details.classList.remove('show9');
+});
+
+document.getElementById('btn-iStudio').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel10');
+  details.classList.toggle('show10');
+});
+
+document.getElementById('close-button10').addEventListener('click', function () {
+  const details = document.getElementById('slide-panel10');
+  details.classList.remove('show10');
+});
